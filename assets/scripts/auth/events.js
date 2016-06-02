e38f161a9cd70988cbcbcf85a1b5cacf34a08ad1
RoobScoobs <iruben229@gmail.com>
2016-06-02 14:32:56 -0400

    Add event handler to show 'X' and 'O'
    Working function that updates the game board with user's appropriate marker
    when clicking

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,7 +6,6 @@ const api = require('./api');
 const ui = require('./ui');
 
 const onSignUp = function (event) {
-  // console.log("You got inside on Sign Up");
   event.preventDefault();
   let data = getFormFields(event.target);
   api.signUp(data)
@@ -37,11 +36,40 @@ const onChangePassword = function (event) {
   .fail(ui.failure);
 };
 
+const onNewGame = function (event) {
+  event.preventDefault();
+  api.newGame()
+  .done(ui.newGameSuccess)
+  .fail(ui.failure);
+};
+
+let turnCount = 0;
+let player1 = 'X';
+let player2 = 'O';
+let empty = '';
+const makeAMove = function (event) {
+    event.preventDefault();
+    if ($(this).text() !== empty){
+      console.log('Make a valid move');
+    } else if (turnCount % 2 === 0) {
+        $(this).text(player1);
+        turnCount++;
+    } else {
+        $(this).text(player2);
+        turnCount++;
+    }
+};
+
+
+
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp);
   $('#sign-in').on('submit', onSignIn);
   $('#sign-out').on('submit', onSignOut);
   $('#change-password').on('submit', onChangePassword);
+  $('#reset-game').on('submit', onNewGame);
+  $('.box').on('click', makeAMove);
+
 };
 
 module.exports = {
